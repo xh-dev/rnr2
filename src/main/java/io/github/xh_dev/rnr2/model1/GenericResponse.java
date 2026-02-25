8edafb492dc11a785f41c829c4db52429e988bf9
xeth hung <Xeth.Hung@aswatson.com>
2026-02-25 13:53:40 +0800

    updates

---
+++ src/main/java/io/github/xh_dev/rnr2/model1/GenericResponse.java
@@ -0,0 +1,14 @@
+package io.github.xh_dev.rnr2.model1;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenericResponse {
+    private String date;
+}
