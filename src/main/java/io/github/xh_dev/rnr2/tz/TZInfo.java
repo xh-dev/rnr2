afb6fbbfb5912a4d296f203ad21aa7689e77d0b6
xeth hung <Xeth.Hung@aswatson.com>
2026-02-26 16:50:25 +0800

    updates

--- src/main/java/io/github/xh_dev/rnr2/tz/TZInfo.java
+++ src/main/java/io/github/xh_dev/rnr2/tz/TZInfo.java
@@ -3,5 +3,6 @@ package io.github.xh_dev.rnr2.tz;
 import java.util.TimeZone;
 
 public class TZInfo {
+    private TZInfo(){}
     public static final TimeZone HKT = TimeZone.getTimeZone("Asia/Hong_Kong");
 }
